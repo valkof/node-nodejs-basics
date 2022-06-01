6b837f7fc5c98b17ce0fbd3e3e9a5a568201d178
valkof <vakofanov@g.bstu.by>
2022-06-02 01:51:45 +0300

    feat: complete task cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
+import { env } from "process";
+
 export const parseEnv = () => {
-    // Write your code here 
+  const arrayKey = [];
+  Object.entries(env).forEach((property) => {
+    arrayKey.push(`RSS_${property[0]}=${property[1]}`);
+  });
+  console.log(arrayKey.join('; '));
 };
+
+parseEnv();
