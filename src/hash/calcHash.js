de4aae4de08b65c320712a2f81ddfdfa13b13601
valkof <vakofanov@g.bstu.by>
2022-06-02 16:14:06 +0300

    feat: complete task hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import { createHash } from "crypto";
+import { readFile } from "fs/promises";
+import { join } from "path";
+import { fileURLToPath, URL } from "url";
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const pathToFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const hashHex = await readFile(pathToFile, 'utf-8').then((content) => {
+    hash.update(content);
+    return hash.digest('hex');
+  }).catch(() => {
+    return null;
+  });
+  return hashHex;
 };
+
+calculateHash().then((hash) => {
+  console.log(hash);
+});
