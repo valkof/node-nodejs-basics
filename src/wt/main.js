5115bddee063623f4dfe73666983a08009308250
valkof <vakofanov@g.bstu.by>
2022-06-05 15:34:06 +0300

    feat: complete task cp

--- src/wt/main.js
+++ src/wt/main.js
@@ -8,25 +8,34 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 export const performCalculations = async () => {
   const numCpus = cpus().length;
   const arrayFibonacci = [];
-  for (let index = 0; index < numCpus; index++) {
-    const worker = new Worker(join(__dirname, 'worker.js'), {workerData: 10 + index});
+  
+  const getResultWorkers = (arr) => {
+    const arrRefactor = arr.sort((a, b) => a.id - b.id).map((el) => {
+      return {status: el.status, data: el.data};
+    })
+    console.log(arrRefactor);
+    return arrRefactor;
+  };
+
+  for (let numWorker = 0; numWorker < numCpus; numWorker++) {
+    const worker = new Worker(join(__dirname, 'worker.js'), {workerData: 10 + numWorker});
     worker.on('message', (value) => {
-      arrayFibonacci.push({id: worker.threadId, status: 'resolved', data: value});
-      if (arrayFibonacci.length === numCpus) {
-        console.log(arrayFibonacci.sort((a, b) => a.id - b.id).map((el) => {
-          return {status: el.status, data: el.data};
-        }));
-      }
+      arrayFibonacci.push({
+        id: worker.threadId,
+        status: 'resolved', data: value
+      });
+      if (arrayFibonacci.length === numCpus) return getResultWorkers(arrayFibonacci);
     });
     worker.on('error', () => {
-      arrayFibonacci.push({id: worker.threadId, status: 'error', data: null});
-      if (arrayFibonacci.length === numCpus) {
-        console.log(arrayFibonacci.sort((a, b) => a.id - b.id).map((el) => {
-          return {status: el.status, data: el.data};
-        }));
-      }
+      arrayFibonacci.push({
+        id: worker.threadId,
+        status: 'error', data: null
+      });
+      if (arrayFibonacci.length === numCpus) return getResultWorkers(arrayFibonacci);
     });
   }
 };
 
-performCalculations();
+(async () => {
+  await performCalculations().then((asd) => {console.log(asd)});
+})();
